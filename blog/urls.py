8f94c3b0ff7a9a8cd6ed1073bfd9cd78d07317ec
JardelPlk <jardel.pauluka14@gmail.com>
2021-06-23 15:40:23 -0300

    Novas atualizações.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
+    path('author/<pk>/', views.author_perfil, name='author_perfil'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]#Lista de urls
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+#from django.http import Http404
+from django.contrib.auth.models import User
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+
+
+
 
 #Regra de negócio
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    users = User.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts, 'users': users})
+
+def post_detail(request, pk):
+    #try:
+    #    post = Post.objects.get(pk=pk)
+    #except Post.DoesNotExist:
+    #    raise Http404('Post missing =/')#Caso a página não exista
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def author_perfil(request, pk):
+    author = get_object_or_404(User, pk=pk)
+    posts = Post.objects.filter(author=author)
+
+    return render(request, 'blog/author_perfil.html', {'author': author, 'posts': posts})
+
+def post_new(request):
+     if request.method == "POST":
+         form = PostForm(request.POST)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm()
+     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+     post = get_object_or_404(Post, pk=pk)
+     if request.method == "POST":
+         form = PostForm(request.POST, instance=post)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm(instance=post)
+     return render(request, 'blog/post_edit.html', {'form': form})
